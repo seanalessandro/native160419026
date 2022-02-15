d750977cd3e925675a82fceb0c85529c35e6daa4
jerrielrhemaldy <85059290+jerrielrhemaldy@users.noreply.github.com>
2022-02-15 14:48:16 +0700

    password added

--- app/src/main/java/id/ac/ubaya/native160419026/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/native160419026/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     //nambahin variabel login
     var login:Boolean = true
-
+    var password:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
